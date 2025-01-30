1ade83edd09b39a480f8d7a4f40b5d8ffab12061
AllanCapistrano <allan.capistrano3@gmail.com>
2025-01-30 20:37:28 -0300

    feat: starting to create the rollback action

---
+++ services/rollback_database.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"fmt"
+	"log"
+)
+
+// Lida com a remoção de um banco de dados
+func handleDeleteDatabase(database string) {
+	query := fmt.Sprintf("DROP DATABASE %s;", database)
+
+	command := handleDdlCommand(query)
+
+	err := command.Run()
+	if err != nil {
+		log.Fatalf("Não foi possível remover o banco de dados %s - %s", database, err)
+	}
+}
+
+// Lida com a criação de um banco de dados vazio
+func handleCreateDatabase(database string) {
+	query := fmt.Sprintf("CREATE DATABASE %s;", database)
+
+	command := handleDdlCommand(query)
+
+	err := command.Run()
+	if err != nil {
+		log.Fatalf("Não foi possível criar o banco de dados %s - %s", database, err)
+	}
+}
+
+// Realiza o rollback de uma migração
+func RollbackDatabase(database string, snapshotFilePath string) {
+	handleDeleteDatabase(database)
+	handleCreateDatabase(database)
+}
