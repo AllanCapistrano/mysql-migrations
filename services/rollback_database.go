26269c7dacb2e46c971def1f58fdefbc1221862d
AllanCapistrano <allan.capistrano3@gmail.com>
2025-01-30 21:02:37 -0300

    chore: adding logs to rollback database function

--- services/rollback_database.go
+++ services/rollback_database.go
@@ -7,6 +7,8 @@ import (
 
 // Lida com a remoção de um banco de dados
 func handleDeleteDatabase(database string) {
+	log.Printf("Removendo o banco de dados '%s'", database)
+
 	query := fmt.Sprintf("DROP DATABASE %s;", database)
 
 	command := handleDdlCommand(query)
@@ -19,6 +21,8 @@ func handleDeleteDatabase(database string) {
 
 // Lida com a criação de um banco de dados vazio
 func handleCreateDatabase(database string) {
+	log.Printf("Criando o banco de dados '%s'", database)
+
 	query := fmt.Sprintf("CREATE DATABASE %s;", database)
 
 	command := handleDdlCommand(query)
@@ -31,6 +35,8 @@ func handleCreateDatabase(database string) {
 
 // Lida com a restauração de um banco de dados
 func handleRestoreDatabase(database string, snapshotFilePath string) {
+	log.Printf("Restaurando o banco de dados '%s'", database)
+
 	command := handleRestoreCommand(snapshotFilePath, database)
 
 	err := command.Run()
@@ -41,7 +47,11 @@ func handleRestoreDatabase(database string, snapshotFilePath string) {
 
 // Realiza o rollback de uma migração
 func RollbackDatabase(database string, snapshotFilePath string) {
+	log.Printf("Iniciando processo de rollback do banco de dados '%s' utilizando a snapshot '%s'", database, snapshotFilePath)
+
 	handleDeleteDatabase(database)
 	handleCreateDatabase(database)
 	handleRestoreDatabase(database, snapshotFilePath)
+
+	log.Println("Processo de rollback finalizado!")
 }
