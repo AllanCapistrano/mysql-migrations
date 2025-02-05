9b4a62a3512543eb24dd6fc9fa9fd86428573628
AllanCapistrano <allan.capistrano3@gmail.com>
2025-02-04 21:36:08 -0300

    feat: creating settings file

---
+++ config/settings.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Settings struct {
+	ContainerName    string   `json:"containerName"`
+	DatabaseUser     string   `json:"databaseUser"`
+	DatabasePassword string   `json:"databasePassword"`
+	WhiteList        []string `json:"whiteList"`
+	BlackList        []string `json:"blackList"`
+}
+
+// Obtém as configurações que foram definidas no arquivo de configurações. Caso
+// não seja possível ler o arquivo, serão utilizadas as configurações padrão.
+func GetSettings(fileName string) Settings {
+	foundSettingsFile := true
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Println("Não foi possível acessar o diretório home do usuário.", err)
+
+		foundSettingsFile = false
+	}
+
+	filePath := filepath.Join(homeDir, ".config", "cnx-migrations", fileName)
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Printf(
+			"Não foi possível ler o arquivo '%s'! Serão utilizados os valores padrão. \n%v.\n",
+			fileName,
+			err,
+		)
+		foundSettingsFile = false
+	}
+
+	defer file.Close()
+
+	if foundSettingsFile {
+		var settings Settings
+
+		decoder := json.NewDecoder(file)
+		err := decoder.Decode(&settings)
+		if err != nil {
+			log.Println("Erro ao decodificar o JSON:", err)
+		}
+
+		return settings
+	}
+
+	return Settings{
+		ContainerName:    "conexa_mysql",
+		DatabaseUser:     "root",
+		DatabasePassword: "root",
+		WhiteList:        []string{},
+		BlackList:        []string{},
+	}
+}
