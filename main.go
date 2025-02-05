bd65f167dc1dc4dc6729c878591bb11ffce6dd5f
AllanCapistrano <allan.capistrano3@gmail.com>
2025-02-04 21:36:39 -0300

    refactor: using the settings file

--- main.go
+++ main.go
@@ -16,15 +16,16 @@ func main() {
 	fmt.Println(databases)
 
 	// database.DumpDatabase("opensev_recorrencia", ".")
-	// database.RollbackDatabase("opensev_recorrencia", "./snapshot_opensev_recorrencia_2025-02-01-1738448077080.sql")
 	// database.ExecuteMigrationsByFile("opensev_recorrencia", "./migration_whatsapp.sql")
+	database.RollbackDatabase("opensev_recorrencia", "./snapshot_opensev_recorrencia_2025-02-04-1738714924138.sql")
 
-	query := `CREATE TABLE whatsapp_mensagens (
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		telefone VARCHAR(15) NOT NULL,
-		mensagem TEXT NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);`
+	// query := `CREATE TABLE whatsapp_mensagens (
+	// 	id INT AUTO_INCREMENT PRIMARY KEY,
+	// 	telefone VARCHAR(15) NOT NULL,
+	// 	mensagem TEXT NOT NULL,
+	// 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	// );`
 
-	database.ExecuteMigrationByQuery("opensev_recorrencia", query)
+	// database.ExecuteMigrationByQuery("opensev_recorrencia", query)
+
 }
--- services/docker/commands.go
+++ services/docker/commands.go
@@ -3,32 +3,44 @@ package docker
 import (
 	"fmt"
 	"os/exec"
+
+	"github.com/AllanCapistrano/cnx-migrations/config"
 )
+
+var settings config.Settings
+
+func init() {
+	settings = config.GetSettings("cnx-migrations.json")
+}
 
 // Prepara a estrutura para a execução de comandos Data Definition Language (DDL)
 func DdlCommand(query string) *exec.Cmd {
-	// TODO: Fazer com que algumas partes sejam configuradas, mas também ter um valor default
+	passwordParameter := fmt.Sprintf("--password=%s", settings.DatabasePassword)
+
 	return exec.Command(
-		"docker", "exec", "conexa_mysql", "mysql", "-u", "root",
-		"--password=root", "-N", "-s", "-e", query,
+		"docker", "exec", settings.ContainerName, "mysql", "-u", settings.DatabaseUser,
+		passwordParameter, "-N", "-s", "-e", query,
 	)
 }
 
 // Comando responsável por realizar o dump do banco de dados.
 func DumpCommand(database string) *exec.Cmd {
-	// TODO: Fazer com que algumas partes sejam configuradas, mas também ter um valor default
+	passwordParameter := fmt.Sprintf("--password=%s", settings.DatabasePassword)
+
 	return exec.Command(
-		"docker", "exec", "conexa_mysql", "mysqldump",
-		"-u", "root", "--password=root", database,
+		"docker", "exec", settings.ContainerName, "mysqldump",
+		"-u", settings.DatabaseUser, passwordParameter, database,
 	)
 }
 
 // Comando responsável por restaurar o dump de um banco de dados.
 func RestoreCommand(dump string, database string) *exec.Cmd {
-	// TODO: Fazer com que algumas partes sejam configuradas, mas também ter um valor default
 	command := fmt.Sprintf(
-		"cat %s | docker exec -i conexa_mysql mysql -u root --password=root %s",
+		"cat %s | docker exec -i %s mysql -u %s --password=%s %s",
 		dump,
+		settings.ContainerName,
+		settings.DatabaseUser,
+		settings.DatabasePassword,
 		database,
 	)
 
@@ -37,9 +49,11 @@ func RestoreCommand(dump string, database string) *exec.Cmd {
 
 // Comando responsável por realizar a migração a partir de um arquivo `.sql`.
 func MigrateByFileCommand(filepath string, database string) *exec.Cmd {
-	// TODO: Fazer com que algumas partes sejam configuradas, mas também ter um valor default
 	command := fmt.Sprintf(
-		"docker exec -i conexa_mysql mysql -u root --password=root %s < %s",
+		"docker exec -i %s mysql -u %s --password=%s %s < %s",
+		settings.ContainerName,
+		settings.DatabaseUser,
+		settings.DatabasePassword,
 		database,
 		filepath,
 	)
