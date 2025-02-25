32da1595b620c2e632c3d512f81dcbe28b5ce5e5
AllanCapistrano <allan.capistrano3@gmail.com>
2025-02-24 22:01:11 -0300

    feat: creating install script

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Baixando os arquivos necessários
+# TODO
+
+# Criando os diretórios
+mkdir -p $HOME/.config/mysql-migrations/.rollback
+
+# Movendo os arquivos para os diretórios corretos
+# TODO
+
+# Dando permissão para a CLI ser executada
+# TODO
+
+# Exibindo os comandos da CLI
+# TODO
